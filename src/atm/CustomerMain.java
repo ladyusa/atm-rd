99225481a7df637d7d1014027fa7708264b4e33f
Usa S <usa.sa@ku.th>
2020-08-25 11:53:31 +0700

    add atm features

--- src/atm/CustomerMain.java
+++ src/atm/CustomerMain.java
@@ -4,7 +4,18 @@ public class CustomerMain {
     public static void main(String[] args) {
         Customer peter = new Customer(1,"Peter","1234");
         Customer nancy = new Customer(2,"Nancy","2345");
-        System.out.println(peter);
-        System.out.println(nancy);
+
+        Bank bank = new Bank("My Bank");
+        bank.addCustomer(peter);
+        bank.addCustomer(nancy);
+
+        ATM atm = new ATM(bank);
+        atm.validateCustomer(1,"1234");
+        atm.deposit(1000);
+        System.out.println(  peter  );
+
+        atm.transfer(2, 200);
+        System.out.println(  peter  );
+        System.out.println(  nancy  );
     }
 }
